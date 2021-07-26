4cc14e8512e465529a7e36a23815bf040fa40fdc
Thor138 <tjcourey84@gmail.com>
2021-07-26 12:00:31 -0600

    columns and createData function first draft

--- client/src/pages/spill_report.js
+++ client/src/pages/spill_report.js
@@ -13,52 +13,75 @@ const columns = [
   { id: "docNumber", label: "Doc Number", minWidth: 170 },
   { id: "subDate", label: "Submit Date", minWidth: 100 },
   { id: "facilityID", label: "Facility ID", minWidth: 100 },
-  { id: "americanPetroleumID", label: "API", minWidth: 100 },
-  {
-    id: "population",
-    label: "Population",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "density",
-    label: "Density",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toFixed(2),
-  },
+  { id: "operator", label: "Operator", minWidth: 100 },
+  { id: "operatorNumber", label: "Operator Number", minWidth: 100 },
+  { id: "facilityType", label: "Facility Type", minWidth: 100 },
+  { id: "groundwaterImpact", label: "Groundwater Impacted", minWidth: 100 },
+  { id: "surfacewaterImpact", label: "Surfacewater Impacted", minWidth: 100 },
+  { id: "closed", label: "Closed", minWidth: 100 },
+  { id: "incidentDate", label: "Incident Date", minWidth: 100 },
+  { id: "resolutionDate", label: "Resolution Date", minWidth: 100 },
+  { id: "latLong", label: "GPS Coordinates", minWidth: 100 },
+  { id: "county", label: "County", minWidth: 100 },
+  { id: "reportUrl", label: "Report URL", minWidth: 100 },
+  { id: "municipality", label: "Municipality", minWidth: 100 },
 ];
-
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
+// change this to match oil spill data
+function createData(
+  docNumber,
+  subDate,
+  facilityID,
+  operator,
+  operatorNumber,
+  facilityType,
+  groundwaterImpact,
+  surfacewaterImpact,
+  closed,
+  incidentDate,
+  resolutionDate,
+  lat,
+  long,
+  county,
+  reportUrl,
+  municipality
+) {
+  const latLong = `${lat} , ${long}`;
+  return {
+    docNumber,
+    subDate,
+    facilityID,
+    operator,
+    operatorNumber,
+    facilityType,
+    groundwaterImpact,
+    surfacewaterImpact,
+    closed,
+    incidentDate,
+    resolutionDate,
+    latLong,
+    county,
+    reportUrl,
+    municipality,
+  };
 }
-
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
-];
+// can use this for temp seed data if values are changed
+// const rows = [
+//   createData("India", "IN", 1324171354, 3287263),
+//   createData("China", "CN", 1403500365, 9596961),
+//   createData("Italy", "IT", 60483973, 301340),
+//   createData("United States", "US", 327167434, 9833520),
+//   createData("Canada", "CA", 37602103, 9984670),
+//   createData("Australia", "AU", 25475400, 7692024),
+//   createData("Germany", "DE", 83019200, 357578),
+//   createData("Ireland", "IE", 4857000, 70273),
+//   createData("Mexico", "MX", 126577691, 1972550),
+//   createData("Japan", "JP", 126317000, 377973),
+//   createData("France", "FR", 67022000, 640679),
+//   createData("United Kingdom", "GB", 67545757, 242495),
+//   createData("Russia", "RU", 146793744, 17098246),
+//   createData("Nigeria", "NG", 200962417, 923768),
+//   createData("Brazil", "BR", 210147125, 8515767),
+// ];
 
 const useStyles = makeStyles({
   root: {
