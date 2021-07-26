3163372487b6944100810a18813c4b0fb55cb303
Jon Gregory <jongregory75@hotmail.com>
2021-07-26 09:55:59 -0600

    state data

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,72 @@
+/* Copyright (c) 2018, 2021, Oracle and/or its affiliates. All rights reserved. */
+
+/******************************************************************************
+ *
+ * You may not use the identified files except in compliance with the Apache
+ * License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * DESCRIPTION
+ *   A basic node-oracledb example using Node.js 8's async/await syntax.
+ *
+ *   For connection pool examples see connectionpool.js and webapp.js
+ *   For a ResultSet example see resultset1.js
+ *   For a query stream example see selectstream.js
+ *
+ *   This example requires node-oracledb 5 or later.
+ *
+ *****************************************************************************/
+
 const oracledb = require("oracledb");
-try {
-  oracledb.initOracleClient({ libDir: "C:\\oracle\\instantclient_19_6" });
-} catch (err) {
-  console.error("Whoops!");
-  console.error(err);
-  process.exit(1);
+
+("use strict");
+
+const oracledb = require("oracledb");
+const dbConfig = require("./dbconfig.js");
+
+// On Windows and macOS, you can specify the directory containing the Oracle
+// Client Libraries at runtime, or before Node.js starts.  On other platforms
+// the system library search path must always be set before Node.js is started.
+// See the node-oracledb installation documentation.
+// If the search path is not correct, you will get a DPI-1047 error.
+if (process.platform === "win64") {
+  // Windows
+  oracledb.initOracleClient({ libDir: "C:\\oracle\\instantclient_19_11" });
+} else if (process.platform === "darwin") {
+  // macOS
+  oracledb.initOracleClient({
+    libDir: process.env.HOME + "/Downloads/instantclient_19_8",
+  });
 }
 
-module.exports = mongoose.connection;
+async function run() {
+  let connection;
+
+  try {
+    // Get a non-pooled connection
+    connection = await oracledb.getConnection(dbConfig);
+
+    console.log("Connection was successful!");
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+}
+
+run();
--- server/server.js
+++ server/server.js
@@ -30,6 +30,20 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
+//start of oracledb connection info
+connection = await oracledb.getConnection({
+  user: process.env.NODE_ORACLEDB_USER,
+  password: process.env.NODE_ORACLEDB_PASSWORD,
+  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING,
+});
+const result = await connection.execute(
+  `SELECT TO_CHAR(CURRENT_DATE, 'DD-Mon-YYYY HH24:MI') AS D FROM DUAL`,
+  [],
+  { outFormat: oracledb.OUT_FORMAT_OBJECT }
+);
+console.log(result);
+// End oracle db connection info
+
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
